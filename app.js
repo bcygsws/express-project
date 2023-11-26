6784d2a23169f1f0fec371cf0c88e6403fc9d7c9
bcygsws <bcygswspring168@gmail.com>
2023-11-27 04:24:16 +0800

    :zap: MySQL事务和锁机制，深度分析018

--- app.js
+++ app.js
@@ -572,6 +572,7 @@ app.use('/api', user);
  * sex ENUM('男','女') default null;
  * 
  * d.默认值约束 default，当没有插入值时，会自动使用默认值 
+ * 
  * e. foreign key 外键约束：两个一致，从表主键和主表外键有同一引用类型（例如：都是int）,同一编码方式，例如：都是utf8
  * 给一张表添加外键的四种方式
  * 参考文档：https://www.cnblogs.com/qisong178878915/p/4435488.html
@@ -579,6 +580,7 @@ app.use('/api', user);
  *  命令：alter table 表名tb1 add constraint fk_id foreign key(id) references 外键表名class_tb(c_id);
  *  e2.第四种方式，也用于为已创建过的表，添加外键
  *  上述命令可简化为：alter table 表名 tb1 add foreign key(id) references 外键表名class_tb(c_id);
+ * 
  * f.primary key 主键约束
  * 
  * 
